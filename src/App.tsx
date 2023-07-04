54b41ad8d1ea6d2bec7c2ba7850dd87e70d146f3
Loriick <loricktravailleur@gmail.com>
2023-07-04 20:59:40 +0200

    show player only if a song was clicked

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import { Suspense } from 'react';
 import Main from './components/Main';
 import Player from './components/Player';
 import { PlayerFallBack } from './components/Player/index.fallback';
+import { useRecoilValue } from 'recoil';
+import { idState, isPlayingState } from './recoil/atoms/player';
 
 function App() {
+  const isPlaying = useRecoilValue(isPlayingState);
+  const currentSongId = useRecoilValue(idState);
   return (
     <>
       <Main />
       <Suspense fallback={<PlayerFallBack />}>
-        <Player />
+        {isPlaying || currentSongId ? <Player /> : null}
       </Suspense>
     </>
   );
